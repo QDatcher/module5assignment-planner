3afe35419b5d06e2b2d5596d199a1ea69edd5218
Quise <darkquise@gmail.com>
2023-03-10 22:09:08 -0500

    created generateTimeBlock function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,8 @@
 let currentDay = dayjs().format('dddd MMM D, YYYY')
 let currentDayEl = $('#currentDay')
 currentDayEl.text(currentDay)
+
+const plannerContainer = $(".container-fluid")
 $(function () {
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
@@ -25,3 +27,27 @@ $(function () {
     // TODO: Add code to display the current date in the header of the page.
   });
   
+  const generateTimeBlock = () => {
+    var timeBlock = $('<div>');
+    timeBlock.addClass('row time-block');
+    var timeLabel = $('<div>')
+    timeLabel.addClass('col-2 col-md-1 hour text-center py-3')
+    timeLabel.text('time')
+    var textBox = $('<textarea>')
+    textBox.addClass('col-8 col-md-10 description')
+    textBox.attr('rows', '3')
+    var saveButton = $('<button>');
+    saveButton.addClass('btn saveBtn col-2 col-md-1');
+    saveButton.attr('aria-label', 'save')
+    var i = $('<i>')
+    i.addClass('fas fa-save')
+    i.attr('aria-hidden', 'true')
+
+    plannerContainer.append(timeBlock)
+    timeBlock.append(timeLabel)
+    timeBlock.append(textBox)
+    timeBlock.append(saveButton)
+    saveButton.append(i)
+  }
+
+  generateTimeBlock()
